00c3e39af1c555b1b38d3f9c8a92e81b263c3346
trofimchuk <trofimchuk@itstep.academy>
2023-09-27 16:36:18 +0300

    Start with redusers

---
+++ src/store/reducers/userReducer/types.ts
@@ -0,0 +1,42 @@
+export interface UserState{
+    user: any,
+    message: null | string,
+    loading: boolean,
+    error: null | string,
+    isAuth: boolean,
+    selectedUser: any,
+    allUsers: []
+}
+
+export enum UserActionTypes {
+    START_REQUEST = "START_REQUEST",
+    FINISHED_REQUEST = "FINISHED_REQUEST",
+    LOGIN_USER_SUCCESS = "LOGIN_USER_SUCCESS",
+    LOGIN_USER_ERROR = "LOGIN_USER_ERROR",
+    SERVER_ERROR = "SERVER_ERROR"
+}
+
+interface StartRequestAction {
+    type: UserActionTypes.START_REQUEST
+}
+
+interface FinishRequestAction {
+    type: UserActionTypes.FINISHED_REQUEST
+}
+
+interface LoginUserSuccessAction{
+    type: UserActionTypes.LOGIN_USER_SUCCESS,
+    payload: any
+}
+
+interface LoginUserErrorAction{
+    type: UserActionTypes.LOGIN_USER_ERROR,
+    payload: any
+}
+
+interface ServerErrorAction{
+    type: UserActionTypes.SERVER_ERROR
+}
+
+export type UserActions =  StartRequestAction | FinishRequestAction | LoginUserSuccessAction | LoginUserErrorAction | ServerErrorAction
+
